a998ccac62ec99fe1335ffad749552fe0c6c66b2
Gaurav Nalawade <gauravnakawade11@gmail.com>
2026-06-07 23:55:00 +0530

    DataTypes in Python

---
+++ DataTypes.py
@@ -0,0 +1,57 @@
+# Integer
+No=11
+print(No)
+print(type(No))
+print(id(No))
+
+# Float
+No2=10.79979
+print(No2)
+print(type(No))
+
+# Complex
+No3=2+2j
+print(No3)
+print(type(No3))
+
+# String
+Str="Gaurav"
+print(Str)
+print(type(Str))
+print(len(Str))
+print(id(Str))
+
+# Set
+Data1={11,21,51,101,111}
+print(Data1)
+print(type(Data1))
+print(len(Data1))
+
+# Tuple
+Data2=(11,21,51,101,111)
+print(Data2)
+print(type(Data2))
+print(len(Data2))
+
+# List
+Data3=[11,21,51,101,111]
+print(Data3)
+print(type(Data3))
+print(len(Data3))
+
+# Dictonary
+dict={"Name":"Gaurav","Age":20}
+print(dict)
+print(type(dict))
+
+# Mone
+Age=None
+print(Age)
+print(type(Age))
+
+# boolean
+bool =True
+print(bool)
+
+bool =False
+print(bool)
