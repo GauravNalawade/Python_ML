099365a3079458c946988dd6b8330bc4f370c55b
Gaurav Nalawade <gauravnakawade11@gmail.com>
2026-07-02 08:35:54 +0530

    Solve question Assignment15_2 and Assignment15_3

---
+++ Assignments/Assignment15/Assignment15_2.py
@@ -0,0 +1,20 @@
+CheckEvenNum = lambda No:(No % 2 == 0)
+
+def main():
+    Data=[]
+
+    print("Enter the Size of List:")
+    Size=int(input())
+
+    print("Enter Elements you want to insert")
+    for i in range(Size):
+        no=int(input())
+        Data.append(no)
+
+    print("Input Data is:",Data)
+
+    FData=list(filter(CheckEvenNum,Data))
+    print("Data After Filter:",FData)
+
+if __name__=="__main__":
+    main()
