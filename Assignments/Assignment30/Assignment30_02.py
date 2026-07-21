cda4e276abbdd71f58d37937702ebae1b0ae0150
Gaurav Nalawade <gauravnakawade11@gmail.com>
2026-07-22 00:20:13 +0530

    Solved 3 questions in assignment30

---
+++ Assignments/Assignment30/Assignment30_02.py
@@ -0,0 +1,17 @@
+import schedule
+import time
+import datetime
+ 
+def CurrentDateTime():
+    CurrentTime=datetime.datetime.now() 
+    print("Current Date and Time : ",CurrentTime.strftime("%d-%m-%Y %I:%M:%S:%p"))
+
+def main():
+     schedule.every(1).minutes.do(CurrentDateTime)
+    
+     while(True):
+          schedule.run_pending()
+          time.sleep(30)
+
+if __name__=="__main__":
+     main()
---
+++ Assignments/Assignment30/Assignment30_03.py
@@ -0,0 +1,16 @@
+import schedule
+import time
+def Codingkar():
+    print("Coding kar.....!")
+
+def main():
+    schedule.every(30).minutes.do(Codingkar)
+
+
+    while(True):
+        schedule.run_pending()
+        time.sleep(10*60) 
+
+if __name__=="__main__":
+    main()
+    
