922401a20d3e069a3391501b7463fd6a1603f819
Gaurav Nalawade <gauravnakawade11@gmail.com>
2026-08-20 22:17:11 +0530

    Modify CaseStudStep8.py and CaseStudyStep9.py files

--- Learning/Machine_Learning/CaseStudyStep8.py
+++ Learning/Machine_Learning/CaseStudyStep8.py
@@ -4,85 +4,86 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split
+
 from sklearn.tree import DecisionTreeClassifier
 
-Border="-"*30
+Border = "-"*30
 
-#########################################
-# Step 1: Load the dataset
-#########################################
+########################################
+# Step 1 : Load the dataset
+########################################
 
 print(Border)
-print("Step 1: Load the dataset")
+print("Step 1 : Load the dataset")
 print(Border)
 
-DataPath="iris.csv"
+DataPath = "iris.csv"
 
-df=pd.read_csv(DataPath)
+df = pd.read_csv(DataPath)
 
-print("Dataset loaded successfully") 
-print("Initial entries from Dataset are.:")
+print("Dataset loaded succefully")
+print("Initial entries from dataset are. : ")
 print(df.head())
 
-#########################################
-# Step 2: Data Analysis (EDA)
-#########################################
+########################################
+# Step 2 : Data Analysis (EDA)
+########################################
 
 print(Border)
-print("Step 2: Data Analysis (EDA)")
+print("Step 2 : Data Analysis (EDA)")
 print(Border)
 
-print("Shape of dataset:",df.shape)
+print("Shape of dataset : ",df.shape)
 
-print("Column names:",list(df.columns))
+print("Column names : ",list(df.columns))
 
-print("Missing value per column:")
-print(df.isnull().sum()) 
+print("Mising values per column : ")
+print(df.isnull().sum())
 
 print("Class distribution (species count)")
 print(df["species"].value_counts())
 
-print("Stastical Reports od dataset")
+print("Statistical report of dataset : ")
 print(df.describe())
 
-#########################################
-# Step 3: Decide Independent and Dependent variables
-#########################################
+########################################
+# Step 3 : Decide Independent & Dependent variables
+########################################
 
 print(Border)
-print("Step 3: Decide Independent and Dependent variables")
+print("Step 3 : Decide Independent & Dependent variables")
 print(Border)
 
-# X : Independent Variable / Features
-# Y : Dependent Variables / Labels
+# X : Independent Varibale / Fetures
+# Y : Dependenet VAriable / Labels
 
-feture_cols=[
+feture_cols = [
     "sepal length (cm)",
     "sepal width (cm)",
     "petal length (cm)",
     "petal width (cm)"
     ]
 
-X=df[feture_cols]
-Y=df["species"]
+X = df[feture_cols]
+Y = df["species"]
 
-print("X Shape:",X.shape) 
-print("Y Shape:",Y.shape)
+print("X Shape : ",X.shape)
+print("Y Shape : ",Y.shape)
 
-#########################################
-# Step 4: visualisation of dataset
-#########################################
+########################################
+# Step 4 : Visualisation of Dataset
+########################################
 
 print(Border)
-print("Step 4: visualisation of dataset")
+print("Step 4 : Visualisation of Dataset")
 print(Border)
 
 # Scatter plot
 plt.figure(figsize=(7,5))
 
 for sp in df["species"].unique():
-    temp=df[df["species"]==sp]
-    plt.scatter(temp["petal length (cm)"],temp["petal width (cm)"],label=sp)
+    temp = df[df["species"] == sp]
+    plt.scatter(temp["petal length (cm)"], temp["petal width (cm)"],label = sp)
 
 plt.title("Marvellous Iris Case Study")
 
@@ -93,67 +94,65 @@ plt.legend()
 plt.grid()
 plt.show()
 
-#########################################
-# Step 5: Split the dataset for training and Testing
-#########################################
-
-
-print(Border)
-print("Step 4: Split the dataset for training and Testing")
-print(Border)
-
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.5,random_state=42)
-
-print("Dataset spliting activity done")
-
-print("X :",X.shape)         # (150,4)
-print("Y :",Y.shape)         # (150,)
-
-print("X_train:",X_train.shape)  # (75,4)
-print("X_test:",X_test.shape)    # (75,4)
-
-print("Y_train:",Y_train.shape)  #(75,)
-print("Y_test:",Y_test.shape)    #(75,)
-
-#########################################
-# Step 6: Build the model
-#########################################
+########################################
+# Step 5 : Split the dataset for training and testing
+########################################
 
 print(Border)
-print("Step 6: Build the model")
+print("Step 5 : Split the dataset for training and testing")
 print(Border)
 
-model=DecisionTreeClassifier(max_depth=5)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.5, random_state=42)
 
+print("Dataset splitting activity done")
 
-print("Model gets created successfully") 
+print("X : ",X.shape)       # (150,4)
+print("Y : ",Y.shape)       # (150,)
 
-#########################################
-# Step 7: Train the model
-#########################################
+print("X_train : ",X_train.shape)   # (75,4)
+print("X_test : ",X_test.shape)     # (75,4)
+
+print("Y_train : ",Y_train.shape)   #(75,)
+print("Y_test : ",Y_test.shape)     #(75,)
+
+########################################
+# Step 6 : Build the model
+########################################
 
 print(Border)
-print("Step 7: Train the model")
+print("Step 6 : Build the model")
+print(Border)
+
+model = DecisionTreeClassifier(max_depth=5)
+
+print("Model gets created succesfully")
+
+########################################
+# Step 7 : Train the model
+########################################
+
+print(Border)
+print("Step 7 : Train the model")
 print(Border)
 
 model.fit(X_train,Y_train)
 
-print("Model trained successfully")
+print("Model trained succesfully")
 
-#########################################
-# Step 8: Evaluate the model
-######################################### 
+########################################
+# Step 8 : Test the model
+########################################
 
 print(Border)
-print("Step 8: Evaluate the model")
-print(Border) 
+print("Step 8 : Test the model")
+print(Border)
 
-Y_pred=model.predict(X_test)
- 
-print("Model Testing done")
+Y_pred = model.predict(X_test)
 
-print("Expected answers:")
+print("Model testing done")
+
+print("Expected answers : ")
 print(Y_test)
 
-print("Predicted answers")
-print(Y_pred) 
+print("Predicted answers : ")
+print(Y_pred)
