26347bdb8fe0e5ad32197ec12c05f32f2c38088d
Gaurav Nalawade <gauravnakawade11@gmail.com>
2026-08-26 01:33:09 +0530

    Completed Assignment No : 39

--- Assignments/Assignment39/Assignment39_01.py
+++ Assignments/Assignment39/Assignment39_01.py
@@ -1,49 +1,73 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
 
 def main():
+    Border="_"*50
 
-    DataFilePath="student_performance_ml.csv"  
+    ##########################################################
+    # Step 1: Load the Dataset 
+    ##########################################################
+    
+    print(Border)
+    print("Step 1: Load the Dataset")
+    print(Border)
 
-    df=pd.read_csv(DataFilePath) 
+    DataPath="student_performance_ml.csv"
+    df=pd.read_csv(DataPath)
 
-    FirstFiveRecords=df.head()
-    print("First 5 Records Are :\n",FirstFiveRecords)
+    ##########################################################
+    # Step 2: Data Analysis (EDA) 
+    ##########################################################
+    print(Border)
+    print("Step 2: Data Analysis (EDA)")
+    print(Border)
 
-    LastFiveRecords=df.tail()
-    print("Last 5 Records Are :\n",LastFiveRecords)
+    print("Shape of dataset:",df.shape)
+    print("Coulumn Names:",list(df.columns))
 
-    rows,columns=df.shape
-    print(f"Total numbers of Rows: {rows}")
-    print(f"Total numbers of Columns: {columns}") 
+    print("Missing values per column:")
+    print(df.isnull().sum())
 
-    print("Data Types of each column")
-    print(df.dtypes) 
+    print("Class Distribution (FinalResult):")
+    print(df["FinalResult"].value_counts())
 
-    # 2
+    print("Statistical report of dataset:")
+    print(df.describe()) 
 
-    print("Total Number of Students:",rows)
+    ##########################################################
+    # Step 3: Visualisation of dataset
+    ##########################################################
 
-    print("Students Passed:",(df['FinalResult']==1).sum())
-    print("Students Failed:",(df['FinalResult']==0).sum())
+    print(Border)
+    print("Step 3: Visualisation of dataset")
+    print(Border)
+     
+    # Scatter Plot
 
-    # 3
-    print("Average StudyHours:",(df['StudyHours'].mean()))
-    print("Average Attendence:",(df['Attendance'].mean()))
-    print("Maximum PreviousScore:",(df['PreviousScore'].max()))
-    print("Minimum SleepHours:",(df['SleepHours'].min()))
+    plt.figure(figsize=(7,5))
 
-    # 4
-    print("Passed and Failed Students:",df['FinalResult'].value_counts())
-    percentage=df['FinalResult'].value_counts(normalize=True) *100
-    print(percentage)
+    for sp in df["FinalResult"].unique():
+        temp=df[df["FinalResult"]==sp]  
+        plt.scatter(temp["StudyHours"],temp["PreviousScore"],label=sp)
 
-    # 6
+    plt.title("Student Pass Fail StudyHours vs PreviousScore")
+    plt.xlabel("StudyHours")
+    plt.ylabel("PreviousScore")
+
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+    # Histogram 
 
     plt.hist(
-        df['StudyHours'],
+        df["StudyHours"],
         bins=4,
         color="skyblue",
         edgecolor="black"
@@ -55,48 +79,186 @@ def main():
 
     plt.show()
 
-    # 7
-    plt.figure(figsize=(7,5))
-
-    for sp in df["FinalResult"].unique():
-        temp = df[df["FinalResult"] == sp]
-        plt.scatter(temp["StudyHours"], temp["PreviousScore"],label = sp)
-
-    plt.title("")
-
-    plt.xlabel("StudyHours")
-    plt.ylabel("PreviousScore")
-
-    plt.legend()
-    plt.grid()
-    plt.show()
-
-    # 8
-    sns.boxplot(y=df['Attendance']) 
-
-    plt.title("BoxPlot for Attendence")
-    plt.show()
-
-    # 9
-    sns.boxplot(  
-        x=df['FinalResult'],
-        y=df['AssignmentsCompleted']
-    )
-    plt.xlabel("Final Result")
-    plt.ylabel("Assignment Completed")
-    plt.title("Assignment Completed VS Final Result")
-    plt.show()
-
-    # 10
+    # Boxplot
+    
     sns.boxplot(
-        x=df['FinalResult'],
-        y=df['SleepHours']
+        x=df["FinalResult"],
+        y=df["AssignmentsCompleted"]
     )
+    plt.title("Boxplot for Assignments Completed Vs Final Result")
     plt.xlabel("Final Result")
-    plt.ylabel("SleepHours")
-    plt.title("SleepHours against FinalResult")
+    plt.ylabel("Assignmet Completed")
     plt.show()
+    
+    
+    ##########################################################
+    # Step 4: Decide Independent and Dependent Variables
+    ##########################################################
+
+    print(Border)
+    print("Step 4: Decide Independent and Dependent Variables")
+    print(Border)
+
+    # features_cols=[
+    #     "StudyHours",  
+    #     "Attendance", 
+    #     "PreviousScore", 
+    #     "AssignmentsCompleted", 
+    #     "SleepHours" 
+    #     ]
+  
+    X=df.drop("FinalResult",axis=1)
+    Y=df["FinalResult"]  
+
+    print("Shape of X :",X.shape)
+    print("Shape of Y :",Y.shape)
+
+    ##########################################################
+    # Step 5: Split Dataset for training and testing 
+    ##########################################################
+
+    print(Border)
+    print("Step 5:Split the dataset for training and testing")  
+    print(Border)
+
+
+    X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.5,random_state=42)
+
+    print("Dataset splitting activity done")  
+
+    print("X_train :",X_train.shape)
+    print("X_test :",X_test.shape)
+
+    print("Y_train :",Y_train.shape)
+    print("Y_test :",Y_test.shape)
+
+    #######################################################################################
+    # Step 6: Build the model with using DecisionTreeClassifier with different max_depths
+    #######################################################################################
+
+    print(Border) 
+    print("Step 6: Build the model with using DecisionTreeClassifier with different max_depths") 
+    print(Border) 
+
+    # max_depth=5
+    model=DecisionTreeClassifier(max_depth=5)
+
+    # max_depth=1
+    model1=DecisionTreeClassifier(max_depth=1) 
+
+    # max_depth=3
+    model3=DecisionTreeClassifier(max_depth=3)
+    
+    # max_depth=None
+    modelN=DecisionTreeClassifier(max_depth=None)
+
+
+    ########################################################## 
+    # Step 7 : Train the models
+    ##########################################################
+
+    print(Border) 
+    print("Step 7: Train the models")
+    print(Border) 
+
+    # max_depth=5
+    model.fit(X_train,Y_train)
+
+    # max_depth=1
+    model1.fit(X_train,Y_train)
+
+    # max_depth=3
+    model3.fit(X_train,Y_train) 
+
+    # max_depth=None
+    modelN.fit(X_train,Y_train) 
+
+
+    ########################################################## 
+    # Step 8 : Evaluate the performance
+    ##########################################################
+
+    print(Border)
+    print(" Step 8 : Evaluate the performance")
+    print(Border)
+
+    # Training Accuracy
+    # print(Border)
+    # print(" Step  : Training Accuracy of model")
+    # print(Border)
+    # Y__train_pred=model.predict(X_train)
+    # Training_Accuracy=accuracy_score(Y_train,Y__train_pred)
+    # print("Training Accuracy:",Training_Accuracy)
+    # print("Actual Values    :", Y_train.to_numpy())
+    # print("Predicted Values :", Y__train_pred)
+
+    
+    # Testing Accuracy
+    print(Border)
+    print("Step  : Testing Accuracy of model")
+    print(Border)
+    Y_Test_pred=model.predict(X_test)
+    Testing_Accuracy=accuracy_score(Y_test,Y_Test_pred)
+    print("Testing Accuracy :",Testing_Accuracy *100)
+    print("Actual Values    :", Y_test.to_numpy())
+    print("Predicted Values :", Y_Test_pred)
+
+    # Testing Accuracy
+    print(Border)
+    print(" Step  : Testing Accuracy of model1")
+    print(Border)
+    Y_model1_Pred=model1.predict(X_test)
+    Accuracy_Score_m1=accuracy_score(Y_test,Y_model1_Pred)
+    print("Testing Accuracy :",Accuracy_Score_m1*100)
+    print("Actual Values    :", Y_test.to_numpy())
+    print("Predicted Values :", Y_model1_Pred)
+
+    # Testing Accuracy
+    print(Border)
+    print(" Step  : Testing Accuracy of model3")
+    print(Border)
+    Y_Pred_model3=model3.predict(X_test)
+    Accuracy_Score_m3=accuracy_score(Y_test,Y_Pred_model3)
+    print("Testing Accuracy :",Accuracy_Score_m3*100)
+    print("Actual Values    :", Y_test.to_numpy())
+    print("Predicted Values :", Y_Pred_model3)
+
+    # Testing Accuracy
+    print(Border)
+    print(" Step  : Testing Accuracy of modelN")
+    print(Border)
+    Y_Pred_modelN=modelN.predict(X_test)
+    Accuracy_Score_mN=accuracy_score(Y_test,Y_Pred_modelN)
+    print("Testing Accuracy :",Accuracy_Score_mN*100)
+    print("Actual Values    :", Y_test.to_numpy())
+    print("Predicted Values :", Y_Pred_modelN)
+
+
+    # Confusion Matrix of Model
+    print(Border)
+    print("Confusion Matrix")
+    print(Border)
+
+    Confusion_matrix=confusion_matrix(Y_test,Y_Test_pred)
+    print("Confusion Matrix is:")
+    print(Confusion_matrix)
+
+    # Predict new record
+    new_student=pd.DataFrame(  
+        {   "StudyHours":[6],
+            "Attendance":[85],
+            "PreviousScore":[66],
+            "AssignmentsCompleted":[7], 
+            "SleepHours":[7]
+        })
+
+    prediction = model.predict(new_student) 
+
+    if prediction[0]==1:
+        print("Predicted result: Pass")
+    else:
+        print("Predicted Result: Fail")
 
 
 if __name__=="__main__":
-    main()
+    main() 
